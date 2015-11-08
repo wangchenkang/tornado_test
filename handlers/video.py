2e4625183c1821f73f8a38a07e3dfacd2c457ebc
CaoYingyong <caoyingyong@xuetangx.com>
2015-11-08 21:18:00 +0800

    学生章节视频观看统计

--- handlers/video.py
+++ handlers/video.py
@@ -44,3 +44,91 @@ class ChapterVideo(BaseHandler):
             }
 
         self.success_response(video_stat)
+
+
+@route('/video/chapter_student_stat')
+class ChapterStudentVideo(BaseHandler):
+    def get(self):
+        course_id = self.get_argument('course_id')
+        chapter_id = self.get_argument('chapter_id')
+        uid = self.get_argument('uid')
+        students = [u.strip() for u in uid.split(',') if u.strip()]
+
+        query = {
+            'query': {
+                'filtered': {
+                    'filter': {
+                        'bool': {
+                            'must': [
+                                {'term': {'course_id': course_id}},
+                                {'term': {'chapter_id': chapter_id}},
+                                {'terms': {'uid': students}},
+                                {'exists': {'field': 'duration'}},
+                                {'exists': {'field': 'study_rate'}}
+                            ]
+                        }
+                    }
+                }
+            },
+            'aggs': {
+                'sequentials': {
+                    'terms': {
+                        'field': 'sequential_id',
+                        'size': 0
+                    },
+                    'aggs': {
+                        'students': {
+                            'terms': {
+                                'field': 'uid',
+                                'size': 0
+                            },
+                            'aggs': {
+                                'videos': {
+                                    'terms': {
+                                        'field': 'vid',
+                                        'size': 0
+                                    },
+                                    'aggs': {
+                                        'record': {
+                                            'top_hits': {
+                                                'size': 1
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            'size': 0
+        }
+
+        data = self.es.search(index='main', doc_type='video', body=query)
+        chapter_student_stat = {}
+        for sequential in data['aggregations']['sequentials']['buckets']:
+            sequential_id = sequential['key']
+            chapter_student_stat.setdefault(sequential_id, {})
+            for student in sequential['students']['buckets']:
+                student_id = student['key']
+                chapter_student_stat[sequential_id].setdefault(student_id, [])
+                for video_item in student['videos']['buckets']:
+                    if video_item['doc_count'] == 0:
+                        continue
+                    student_record = video_item['record']['hits']['hits'][0]
+                    student_video_item = {
+                        'video_id': student_record['_source']['vid'],
+                        'review': student_record['_source']['review'],
+                        'review_rate': student_record['_source']['review_rate'],
+                        'watch_num': student_record['_source']['watch_num'],
+                        'duration': student_record['_source']['duration']
+                    }
+                    chapter_student_stat[sequential_id][student_id].append(student_video_item)
+
+        result = {
+            'total': data['hits']['total'],
+            'sequentials': chapter_student_stat
+        }
+
+        self.success_response(result)
+
