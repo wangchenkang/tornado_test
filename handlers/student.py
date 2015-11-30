7ea45833ce947fef25d5c42ce5a43d26caa2d31c
CaoYingyong <caoyingyong@xuetangx.com>
2015-11-30 05:46:48 +0000

    students binding org api

--- handlers/student.py
+++ handlers/student.py
@@ -3,3 +3,45 @@ from .base import BaseHandler
 from utils.routes import route
 
 
+@route('/student/binding_org')
+class StudentOrg(BaseHandler):
+    """ 
+    获取学堂选修课学生列表
+    """
+    def get(self):
+        course_id = self.course_id
+        org = self.get_param('org')
+    
+        default_size = 100000
+        query = { 
+            'query': {
+                'filtered': {
+                    'filter': {
+                        'bool': {
+                            'must': [
+                                {'term': {'course_id': course_id}},
+                                {'term': {'courses': org}},
+                            ]   
+                        }   
+                    }   
+                }   
+            },  
+            'size': default_size
+        }   
+
+        data = self.es_search(index='main', doc_type='student', body=query)
+        if data['hits']['total'] > default_size:
+            query['size'] = data['hits']['total']
+            data = self.es_search(index='main', doc_type='student', body=query)
+
+        students = []
+        for item in data['hits']['hits']:
+            students.append({
+                'user_id': int(item['_source']['uid']),
+                'binding_id': item['_source'].get('binding_uid', None),
+                'binding_org': item['_source'].get('binding_org', None),
+            })
+
+        self.success_response({'students': students})
+
+
