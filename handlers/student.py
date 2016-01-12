1e8d19eb01fb07d7121312a7161c874852164019
CaoYingyong <caoyingyong@xuetangx.com>
2016-01-12 06:23:35 +0000

    添加星球大战学生统计接口

--- handlers/student.py
+++ handlers/student.py
@@ -2,7 +2,7 @@
 from .base import BaseHandler
 from utils.routes import route
 from utils.log import Log
-
+from utils.tools import date_to_str
 
 Log.create('student')
 
@@ -104,3 +104,135 @@ class StudyStudentList(BaseHandler):
         Log.error(grade_result)
 
         self.success_response({'students': ''})
+
+
+@route('/student/study_period')
+class StudyPeriod(BaseHandler):
+    """
+    查询学生学习时段
+    """
+    def get(self):
+        user_id = self.get_param('user_id')
+
+        query = self.es_query(index='rollup', doc_type='user_study_period') \
+                .filter('term', user_id=user_id)
+
+        periods = {
+            '0': '[00:00~06:00)',
+            '1': '[06:00~12:00)',
+            '2': '[12:00~18:00)',
+            '3': '[18:00~24:00)',
+        }
+
+        data = self.es_execute(query)
+        try:
+            hit = data.hits[0]
+        except IndexError:
+            hit = {}
+
+        user_periods = {
+            '0': getattr(hit, '0', 0),
+            '1': getattr(hit, '1', 0),
+            '2': getattr(hit, '2', 0),
+            '3': getattr(hit, '3', 0),
+        }
+
+        self.success_response({'periods': periods, 'user_periods': user_periods})
+
+
+@route('/student/information')
+class StudentInformation(BaseHandler):
+    """
+    查询学生基本信息
+    """
+    def get(self):
+        user_id = self.get_param('user_id')
+
+        user_sum_query = self.es_query(index='main', doc_type='user_sum') \
+                .filter('term', user_id=user_id)[:0]
+        user_sum_query.aggs.metric('post_total', 'sum', field='post_number')
+        user_sum_query.aggs.metric('comment_total', 'sum', field='comment_number')
+
+        data = self.es_execute(user_sum_query)
+
+        post_total = int(data.aggregations.post_total.value)
+        comment_total = int(data.aggregations.comment_total.value)
+
+        enrollment_query = self.es_query(index='main', doc_type='enrollment') \
+                .filter('term', uid=user_id).filter('term', is_active=True)[:10000]
+        enrollment_data = self.es_execute(enrollment_query)
+        courses = []
+        for item in enrollment_data.hits:
+            courses.append(item.course_id)
+
+        self.success_response({
+            'post_total': post_total,
+            'comment_total': comment_total,
+            'courses': courses
+        })
+
+
+@route('/student/courses')
+class StudentCourses(BaseHandler):
+    def get(self):
+        user_id = self.get_param('user_id')
+
+        user_sum_query = self.es_query(index='main', doc_type='user_sum') \
+                .filter('term', user_id=user_id)[:0]
+        user_sum_query.aggs.bucket('course', 'terms', field='course_id') \
+                .metric('post_total', 'sum', field='post_number') \
+                .metric('comment_total', 'sum', field='comment_number')
+
+        data = self.es_execute(user_sum_query)
+
+        enrollment_query = self.es_query(index='main', doc_type='enrollment') \
+                .filter('term', uid=user_id).filter('term', is_active=True)[:10000]
+        enrollment_data = self.es_execute(enrollment_query)
+
+        video_rate_query = self.es_query(index='rollup', doc_type='course_video_rate') \
+                .filter('term', uid=user_id)[:10000]
+        video_rate_data = self.es_execute(video_rate_query)
+
+        video_query = self.es_query(index='main', doc_type='video') \
+                .filter('term', uid=user_id)
+        default_size = 100000
+        video_data = self.es_execute(video_query[:default_size])
+        if video_data.hits.total > default_size:
+            video_data = self.es_execute(video_query[:video_data.hits.total])
+
+        courses = {}
+        for item in enrollment_data.hits:
+            courses[item.course_id] = {
+                'course_id': item.course_id,
+                'time': item.event_time,
+                'post_total': 0,
+                'comment_total': 0,
+                'video_rate': 0
+            }
+
+        for item in data.aggregations.course.buckets:
+            if item.key not in courses:
+                continue
+            courses[item.key].update({
+                'post_total': int(item.post_total.value),
+                'comment_total': int(item.comment_total.value)
+            })
+
+        for item in video_rate_data.hits:
+            if item.course_id not in courses:
+                continue
+            courses[item.course_id].update({
+                'video_rate': round(float(item.study_rate_open), 4)
+            })
+
+        for item in video_data.hits:
+            if item.course_id not in courses:
+                continue
+            courses[item.course_id].setdefault('video_count', 0)
+            courses[item.course_id].setdefault('video_length', 0)
+            item_study_length = int(int(item.duration) * float(item.study_rate))
+            if item_study_length > 0:
+                courses[item.course_id]['video_count'] += 1
+                courses[item.course_id]['video_length'] += item_study_length
+
+        self.success_response({'courses': courses.values()})
