0f23ef33106ec398957e3acf58c561d7691ade27
yangshuo <yangshuo@xuetangx.com>
2016-08-22 17:25:46 +0800

    add grade interfaces

--- handlers/detail.py
+++ handlers/detail.py
@@ -9,9 +9,16 @@ Log.create('detail')
 
 @route('/detail/course_grade_ratio')
 class DetailCourseGradeRatio(BaseHandler):
+    """
+    学生在某门课程中的得分率的平均值和标准差
+    计算时需要去掉grade_ratio=-2的学生，即参加课程但没有答题的学生
+    http://confluence.xuetangx.com/pages/viewpage.action?pageId=9044555 1.1关键指标
+    http://confluence.xuetangx.com/pages/viewpage.action?pageId=9044555 1.2关键指标
+    """
     def get(self):
         query = self.es_query(index='tap', doc_type='problem_course') \
-                .filter('term', course_id=self.course_id)
+                .filter('term', course_id=self.course_id) \
+                .filter('range', final_grade={'gte': 0})
         problem_users = self.get_problem_users()
         query.filter('terms', user_id=problem_users)
 
@@ -19,44 +26,43 @@ class DetailCourseGradeRatio(BaseHandler):
         result = self.es_execute(query[:result.hits.total])
         hits = result.hits
 
-        problem_user_count = 0
-        grade_ratio_sum = 0
-        problem_user_grade_ratio_list = []
-        for hit in hits:
-            if hit.grade_ratio == -2:
-                continue
-            grade_ratio_sum += hit.grade_ratio
-            problem_user_count += 1
-            problem_user_grade_ratio_list.append(hit.grade_ratio)
+        grade_list = [hit.final_grade for hit in hits]
 
         grade_overview = {'mean': 0, 'variance': 0}
-        if problem_user_count:
-            grade_overview['mean'] = float(grade_ratio_sum) / problem_user_count
-            grade_overview['variance'] = var(problem_user_grade_ratio_list)
+        if hits.total:
+            grade_overview['mean'] = sum(grade_list) / hits.total
+            grade_overview['variance'] = var(grade_list)
 
-        Log.debug(grade_overview)
         self.success_response({'data': grade_overview})
 
 
 @route('/detail/student_grade_ratio')
 class DetailCourseGradeRatioDetail(BaseHandler):
+    """
+    课程所有学生的得分率(得分率>=0的）
+    http://confluence.xuetangx.com/pages/viewpage.action?pageId=9044555 1.1图
+    """
     def get(self):
         query = self.es_query(index='tap', doc_type='problem_course') \
                 .filter('term', course_id=self.course_id) \
-                .filter('range', **{'grade_ratio': {'gt': 0}})
+                .filter('range', **{'final_grade': {'gte': 0}})
 
         response = self.es_execute(query[:0])
         response = self.es_execute(query[:response.hits.total])
 
         result = {}
-        for user in response.hits:
-            result[user.user_id] = user.grade_ratio
+        for hit in response.hits:
+            result[hit.user_id] = hit.final_grade
 
         self.success_response({'data': result})
 
 
 @route('/detail/student_study_ratio')
 class DetailCourseStudyRatioDetail(BaseHandler):
+    """
+    课程所有学生的学习比例
+    http://confluence.xuetangx.com/pages/viewpage.action?pageId=9044555 1.1图
+    """
     def get(self):
         query = self.es_query(index='tap', doc_type='video_course') \
             .filter('term', course_id=self.course_id)
@@ -73,6 +79,10 @@ class DetailCourseStudyRatioDetail(BaseHandler):
 
 @route('/detail/course_study_ratio')
 class DetailCourseStudyRatio(BaseHandler):
+    """
+    课程学习比例的均值和方差
+    http://confluence.xuetangx.com/pages/viewpage.action?pageId=9044555 1.1关键指标
+    """
     def get(self):
         query = self.es_query(index='rollup', doc_type='course_video_rate') \
                 .filter('term', course_id=self.course_id)
@@ -103,6 +113,11 @@ class DetailCourseStudyRatio(BaseHandler):
 
 @route('/detail/course_discussion')
 class DetailCourseDiscussion(BaseHandler):
+    """
+    课程的发帖/回帖总数和平均数
+    http://confluence.xuetangx.com/pages/viewpage.action?pageId=9044555 1.1关键指标
+    http://confluence.xuetangx.com/pages/viewpage.action?pageId=9044555 1.3关键指标
+    """
     def get(self):
         query = self.es_query(index='tap', doc_type='discussion_aggs') \
                 .filter('term', course_id=self.course_id)
@@ -123,8 +138,55 @@ class DetailCourseDiscussion(BaseHandler):
         self.success_response({'data': result})
 
 
+@route('/detail/homework_grade')
+class DetailHomeworkGrade(BaseHandler):
+    """
+    每次作业的得分率均值
+    http://confluence.xuetangx.com/pages/viewpage.action?pageId=9044555 1.2关键指标 图
+    """
+    def get(self):
+
+        query = self.es_query(index='tap', doc_type='seq_problem') \
+            .filter('term', course_id=self.course_id)
+
+        response = self.es_execute(query[:0])
+        response = self.es_execute(query[:response.hits.total])
+
+        result = {}
+        for hit in response.hits:
+            seq_id = hit['seq_id']
+            if seq_id not in result:
+                result[seq_id] = {'sum_grade': 0, 'student_num': 0}
+                result[seq_id]['distribution'] = {}
+            result[seq_id]['sum_grade'] += hit['grade']
+            result[seq_id]['student_num'] += 100
+            if hit['grade'] not in result[seq_id]['distribution']:
+                result[seq_id]['distribution'][hit['grade']] = 0
+            result[seq_id]['distribution'][hit['grade']] += 1
+
+        for seq in result.keys():
+            result[seq]['grade_ratio_mean'] = round((float(result[seq]['sum_grade']) / result[seq]['student_num']) * 100)
+            del result[seq]['sum_grade']
+            del result[seq]['student_num']
+
+        self.success_response({'data': result})
+
+
+@route('/detail/homework_grade_detail')
+class DetailStudentDiscussion(BaseHandler):
+    """
+    
+    """
+    def get(self):
+        pass
+
+
 @route('/detail/student_discussion')
 class DetailStudentDiscussion(BaseHandler):
+    """
+    课程所有学生的发帖数，回帖数，总帖子数
+    http://confluence.xuetangx.com/pages/viewpage.action?pageId=9044555 1.1图
+    """
     def get(self):
         query = self.es_query(index='tap', doc_type='discussion_aggs') \
                 .filter('term', course_id=self.course_id)
@@ -134,27 +196,33 @@ class DetailStudentDiscussion(BaseHandler):
 
         result = {}
         for item in response.hits:
-            result[item.user_id] = item.post_num + item.reply_num
+            result[item.user_id] = {}
+            result[item.user_id]['post_num'] = item.post_num
+            result[item.user_id]['reply_num'] = item.reply_num
+            result[item.user_id]['total_num'] = item.post_num + item.reply_num
 
         self.success_response({'data': result})
 
-#TODO
+
 @route('/detail/student_discussion_stat')
 class DetailStudentDiscussionStat(BaseHandler):
+    """
+    按总帖子数进行分组，每组的学生人数
+    http://confluence.xuetangx.com/pages/viewpage.action?pageId=9044555 1.3图
+    """
     def get(self):
         query = self.es_query(index='tap', doc_type='discussion_aggs') \
                 .filter('term', course_id=self.course_id)
-        # need to add post_total field
-        query.aggs.bucket('post', 'terms', field='post_total', size=0) \
 
-        response = self.es_execute(query)
+        response = self.es_execute(query[:0])
+        response = self.es_execute(query[:response.hits.total])
 
-        result = []
-        for item in response.aggregations.post.buckets:
-            result.append({
-                'post_num': item['key'],
-                'student_num': item.doc_count
-            })
+        result = {}
+        for hit in response.hits:
+            total_num = hit['post_num'] + hit['reply_num']
+            if total_num not in result:
+                result[total_num] = 0
+            result[total_num] += 1
 
         self.success_response({'data': result})
 
@@ -162,6 +230,10 @@ class DetailStudentDiscussionStat(BaseHandler):
 #TODO
 @route('/detail/chapter_study_ratio')
 class DetailChapterStudyRatio(BaseHandler):
+    """
+    每一章的学习比例均值
+    http://confluence.xuetangx.com/pages/viewpage.action?pageId=9044555 1.4关键指标
+    """
     def get(self):
         query = self.es_query(index='tap', doc_type='video_chapter') \
                 .filter('term', course_id=self.course_id)
@@ -182,6 +254,10 @@ class DetailChapterStudyRatio(BaseHandler):
 #TODO
 @route('/detail/chapter_study_ratio_detail')
 class DetailChapterStudyRatioDetail(BaseHandler):
+    """
+    每一章的学习比例分组(0-10|11-20...)的人数
+    http://confluence.xuetangx.com/pages/viewpage.action?pageId=9044555 1.4图
+    """
     def get(self):
         query = self.es_query(index='tap', doc_type='video_chapter') \
                 .filter('term', course_id=self.course_id)
