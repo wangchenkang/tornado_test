c37174d5791277afbbb9015aeac611dfa6e11c6a
wangchenkang <wangchenkang@xuetangx.com>
2017-05-17 09:02:59 +0000

    add download_date

--- handlers/config.py
+++ handlers/config.py
@@ -23,3 +23,21 @@ class DataDateConfig(BaseHandler):
             })
         except (IndexError, AttributeError):
             self.error_response(101, u'数据时间配置错误')
+
+@route('/download/data_date')
+class DownloadDataDate(BaseHandler):
+    """
+    获取任务job的最新时间
+    """
+    def get(self):
+        query = self.es_query(index='download', doc_type='data_conf')
+        response =self.es_execute(query)
+
+        try:
+            config = response[0]
+            self.success_response({
+                    'log_start_date': config.log_start_date,
+                    'latest_data_date': config.latest_data_date
+                    })
+        except (IndexError, AttributeError):
+            self.error_response(101, u'数据时间配置错误')
