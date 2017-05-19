61f01dda93ba2774d2d496e94cb6fc9f1f26cee4
wangchenkang <wangchenkang@xuetangx.com>
2017-05-19 02:08:09 +0000

    Fix discussion

--- handlers/discussion.py
+++ handlers/discussion.py
@@ -432,7 +432,7 @@ class CourseDiscussionStat(BaseHandler):
 
     def get(self):
         result = {}
-        query = self.es_query(index='test_comment', doc_type='study_comment_problem') \
+        query = self.es_query(doc_type='study_comment_problem') \
                 .filter('term', course_id=self.course_id) \
                 .filter("term", group_key=self.group_key) \
                 .filter('term', chapter_id=self.chapter_id)[:1]
