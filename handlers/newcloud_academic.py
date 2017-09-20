eb6ce45f536fc893b6d30c6811e3b611beebbc41
wangchenkang <wangchenkang@xuetangx.com>
2017-09-20 09:46:57 +0000

    Add newcloud_academic

--- handlers/newcloud_academic.py
+++ handlers/newcloud_academic.py
@@ -22,6 +22,83 @@ class AcademicData(BaseHandler):
         data = self.get_param(key)
         return param
 
+    def get_total_page(self, total, num):
+        if total == 0:
+            total_page = 0
+        elif total <= num:
+            total_page = 1
+        else:
+            if total % page == 0:
+                total_page = total/page
+            else:
+                total_page = total/page + total%page
+
+        return total_page
+
+    def get_course_num(self, buckets):
+        unopen_num = 0
+        open_num = 0
+        close_num = 0
+        for bucket in buckets:
+            if bucket.key == 'unopen_num':
+                unopen_num = bucket.doc_count
+            elif bucket.key == 'open_num':
+                open_num = bucket.doc_count
+            else:
+                close_num = bucket.doc_count
+        return unopen_num, open_num, close_num
+
+    @gen.coroutine   
+    def add_course_image(self, results):
+        data = []
+        for result in results:
+            course_info = result.to_dict()
+            course_detail = yield self.course_detail(result.course_id)
+            image_url = course_detail['image_url']
+            course_info['image_url'] = image_url
+            data.append(course_info)
+        raise gen.Return(data)
+
+    @property
+    def course_query(self):
+        org_id = self.get_param('org_id')
+        term_id = self.get_param('term_id')
+        service_line = self.get_param('service_line')
+        course_status = self.get_param('course_status')
+        query = self.es_query(index = settings.NEWCLOUD_ACADEMICS_ES_INDEX, doc_type = 'course_info')\
+                             .filter('term', org_id = org_id)\
+                             .filter('term', term_id = term_id)
+        if service_line != 'all':
+            query = query.filter('term', service_line = service_line) 
+        if course_status != 'all':
+            query = query.filter('term', course_status = course_status)
+       
+        return query
+
+    def teacher_query(self):
+        pass
+
+    def student_query(self, status=False):
+        org_id = self.get_param('org_id')
+        faculty = self.get_param('faculty')
+        major = self.get_param('major')
+        cohort = self.get_param('cohort')
+        entrance_year = self.get_param('entrance_year')
+
+        if status:
+            query = self.es_query(index = settings.NEWCLOUD_ACADEMIC_ES_INDEX, doc_type = 'student_info')
+        else:
+            query = self.es_query(index = settings.NEWCLOUD_ACADEMIC_ES_INDEX, doc_type = 'student_enroll')
+
+        query = query.filter('term', org_id = org_id)\
+                     .filter('term', faculty = faculty)\
+                     .filter('term', major = major)\
+                     .filter('term', cohort = cohort)\
+                     .filter('term', entrance_year = entrance_year)
+        
+        return query
+
+
 @route('/course/overview')
 class CourseOverview(AcademicData):
     """
@@ -29,20 +106,8 @@ class CourseOverview(AcademicData):
     """
     @property 
     def query(self):
-        org_id = self.get_param('org_id')
-        #TODO
-        term = self.get_param('term_id')
-        service_line = self.get_param('service_line')
-        course_status = self.get_param('course_status')
-       
-        query = self.es_query(index = settings.NEWCLOUD_ACADEMICS_ES_INDEX, doc_type = 'course_info')\
-                             .filter('term', org_id = org_id)\
-                             .filter('term', term = term)
+        query = self.course_query
         total = self.es_execute(query[:0]).hits.total
-        if service_line != 'all':
-            query = query.filter('term', service_line = service_line) 
-        if course_status != 'all':
-            query = query.filter('term', course_status = course_status)
         query.aggs.bucket('per_course_status', 'terms', field='course_status', size=total or 1)
         query.aggs.metric('video_total', 'sum', field='video_total_course')
         query.aggs.metric('video_avg', 'avg', field='video_total_course')
@@ -57,38 +122,30 @@ class CourseOverview(AcademicData):
         result = self.es_execute(query)
         aggs = result.aggregations    
         course_num = result.hits.total
-        unopen_num = 0
-        open_num = 0
-        close_num = 0
-        for bucket in aggs.per_course_status.buckets:
-            if bucket.key == 'unopen_num':
-                unopen_num = bucket.doc_count
-            elif bucket.key == 'open_num':
-                open_num = bucket.doc_count
-            else:
-                close_num = bucket.doc_count
+        buckets = aggs.per_course_status.buckets
+        unopen_num, open_num, close_num = self.get_course_num(buckets)
         video_total = aggs.video_total.value
         video_avg = aggs.video_avg.value
         discussion_total = aggs.discussion_total.value
         discussion_avg = aggs.discussion_avg.value
         accomplish_percent = aggs.accomplish_percent.value
         correct_percent = aggs.correct_percent.value
+        
         data = {}
-        data['course_num'] = course_num or 0
-        data['unopen_num'] = unopen_num or 0
-        data['open_num'] = open_num or 0
-        data['close_num'] = close_num or 0
-        data['video_total'] = video_total or 0
+        data['course_num'] = course_num
+        data['unopen_num'] = unopen_num
+        data['open_num'] = open_num
+        data['close_num'] = close_num
+        data['video_total'] = video_total
         data['video_avg'] = video_avg or 0
-        data['discussion_total'] = discussion_total or 0
+        data['discussion_total'] = discussion_total
         data['discussion_avg'] = discussion_avg or 0
         data['accomplish_percent'] = accomplish_percent or 0
         data['correct_percent'] = correct_percent or 0
+       
         return data
 
-    @gen.coroutine
     def get(self):
-        #TODO
         query = self.query
         result = self.get_result(query)
         self.success_response({'data': result})
@@ -101,39 +158,22 @@ class CourseList(AcademicData):
     """
     @property
     def query(self):
-        org_id = self.get_param('org_id')
-        #TODO 
-        term = self.get_param('term_id')
-        service_line = self.get_param('service_line')
-        course_status = self.get_param('course_status').decode('utf-8')
         sort = self.get_argument('sort', 'start')
         sort_type = self.get_argument('sort_type', 0)
         sort = '%s' % sort if sort_type else '-%s' % sort
-        query = self.es_query(index = settings.NEWCLOUD_ACADEMICS_ES_INDEX, doc_type = 'course_info')\
-                            .filter('term', orgid_or_host = org_id)\
-                            .filter('term', term = term)\
-                            .source(COURSE_FIELD)\
-                            .sort(sort)
 
-        if service_line != 'all':
-            query = query.filter('term', service_line = service_line)
-        if course_status != 'all':
-            query = query.filter('term', course_status = course_status)
+        query = self.course_query
+        query = query.source(COURSE_FIELD)\
+                     .sort(sort)
         
         return query
 
     def get_result(self, query, page, num):
         total = self.es_execute(self.query[:0]).hits.total
-        if total == 0:
-            total_page = 0
-        elif total <= num:
-            total_page = 1
-        else:
-            if total % page == 0:
-                total_page = total/page
-            else:
-                total_page = total/page + total%page
+        total_page = self.get_total_page
+
         result = self.es_execute(self.query[(page-1)*num: page*num]).hits
+
         return result, total, total_page
 
     @gen.coroutine
@@ -142,15 +182,9 @@ class CourseList(AcademicData):
         num = self.get_argument('num', 5)
         query = self.query
         results, total, total_page = self.get_result(query, page, num)
-        data = []
-        for result in results:
-            course_info = result.to_dict()
-            course_detail = self.course_detail(result.course_id)
-            image_url = '' if not course_detail else course_detail['image_url']
-            course_name = '' if not course_detail else course_detail['display_name']
-            course_info['image_url'] = image_url
-            course_info['course_name'] = course_name
-            data.append(course_info)
+
+        data = yield self.add_course_image(results)
+
         self.success_response({'data': data, 'total_page': total_page, 'course_num': total, 'current_page': page})
 
 
@@ -165,7 +199,7 @@ class TeacherOverview(AcademicData):
 
         query = self.es_query(index = settings.NEWCLOUD_ACADEMICS_ES_INDEX, doc_type='xxx')\
                             .filter('term', org_id = org_id)\
-                            .filter('term', faculty = faculty)\
+                            .filter('term', faculty = faculty)
         return query
     
     def get_result(self, term):
@@ -205,18 +239,7 @@ class StudentOverview(AcademicData):
     """
     @property
     def query(self):
-        org_id = self.get_param('org_id')
-        faculty = self.get_param('faculty')
-        major = self.get_param('major')
-        cohort = self.get_param('cohort')
-        entrance_year = self.get_param('entrance_year')
-
-        query = self.es_query(index = settings.NEWCLOUD_ACADEMIC_ES_INDEX, doc_type = 'student_enroll')\
-                                .filter('term', org_id = org_id)\
-                                .filter('term', faculty = faculty)\
-                                .filter('term', major = major)\
-                                .filter('term', cohort = cohort)\
-                                .filter('term', entrance_year = entrance_year)
+        query = self.student_query
         query.aggs.metric('enroll_total', 'sum', field = 'participate_total_user')
         query.aggs.metric('enroll_avg', 'avg', field = 'participate_total_user')
         query.aggs.metric('discussion_total', 'sum', field = 'discussion_num')
@@ -261,41 +284,21 @@ class StudentList(AcademicData):
     """
     @property
     def query(self):
-        org_id = self.get_param('org_id')
-        faculty = self.get_param('faculty')
-        major = self.get_param('major')
-        cohort = self.get_param('cohort')
-        entrance_year = self.get_param('entrance_year')
+        query = self.student_query
+        query.source(STUDENT_FIELD)\
+             .sort('user_id')
 
-        query = self.es_query(index = settings.NEWCLOUD_ACADEMIC_ES_INDEX, doc_type = 'student_enroll')\
-                                .filter('term', org_id = org_id)\
-                                .filter('term', faculty = faculty)\
-                                .filter('term', major = major)\
-                                .filter('term', cohort = cohort)\
-                                .source(STUDENT_FIELD)\
-                                .sort('user_id')
         return query
 
-    def get_result(self,query, page, num, student_keyword):
+    def get_result(self, query, page, num, student_keyword):
         if student_keyword != 'all':
             query = query.filter(Q('bool', should=[Q('wildcard', rname = '%s*' % student_keyword)\
                                                  | Q('wildcard', binding_uid = '%s*' % student_keyword)
                                                    ]))
-        else:
-            pass
 
         total = self.es_execute(query[:0]).hits.total
+        total_page = self.get_total_page(total, num)
         
-        if total == 0:
-            total_page = 0
-        elif total <= num:
-            total_page =1
-        else:
-            if total%num == 0:
-                total_page = total/num
-            else:
-                total_page = total/num + 1
-
         results = self.es_execute(query[(page-1)*num: page*num]).hits
         #TODO
         data = [result.to_dict() for result in results]
@@ -318,21 +321,8 @@ class StudentDetailOverview(AcademicData):
     """
     @property
     def query(self):
-        org_id = self.get_param('org_id')
-        term = self.get_param('term')
-        faculty = self.get_param('faculty')
-        major = self.get_param('major')
-        cohort = self.get_param('cohort')
-        entrance_year = self.get_param('entrance_year')
         user_id = self.param('user_id')
-
-        query = self.es_query(index = settings.NEWCLOUD_ACADEMIC_ES_INDEX, doc_type = 'student_info')\
-                                .filter('term', org_id = org_id)\
-                                .filter('term', term = term)\
-                                .filter('term', faculty = faculty)\
-                                .filter('term', major = major)\
-                                .filter('term', cohort = cohort)\
-                                .filter('term', entrance_year = entrance_year)
+        query = self.student_query(status=True)
 
         query_user = query.filter('term', user_id = user_id)
         query_user.aggs.metric('course_total', 'sum', field = 'course_id')
@@ -354,17 +344,8 @@ class StudentDetailOverview(AcademicData):
         aggs = result.aggregations
 
         course_total = aggs_user.course_total.value
-        open_num = 0
-        unopen_num = 0
-        close_num = 0
-        for bucket in aggs_user.per_course_status.buckets:
-            if bucket.key == 'open_num':
-                open_num = bucket.doc_count
-            if bucket.key == 'unopen_num':
-                unopen_num = bucket.doc_count
-            if bucket.key == 'close_num':
-                close_num = bucket.doc_count
-        buckets = aggs.user_total.buckets
+        buckets = aggs_user.per_course_status.buckets
+        unopen_num, open_num, close_num = self.get_course_num(buckets)
         student_num = len(buckets)
         study_video_total = aggs_user.study_video_total.value
         study_video_avg = round(study_video_total/student_num or 1, 2)
@@ -394,58 +375,34 @@ class StudentDetailOverview(AcademicData):
 
 
 @route('/student/detail/courses')
+class StudentDetailCourse(AcademicData):
     """
     学生概况，学生详情页课程列表信息
     """
     @property
     def query(self):
-        org_id = self.get_param('org_id')
-        term = self.get_param('term')
-        faculty = self.get_param('faculty')
-        major = self.get_param('major')
-        cohort = self.get_param('cohort')
-        entrance_year = self.get_param('entrance_year')
-        user_id = self.param('user_id')
+        user_id = self.get_param('user_id')
+        query = self.student_query(status=True)
+        query = query.filter('term', user_id = user_id)
 
-        query = self.es_query(index = settings.NEWCLOUD_ACADEMIC_ES_INDEX, doc_type = 'student_course_info')\
-                                .filter('term', org_id = org_id)\
-                                .filter('term', term = term)\
-                                .filter('term', faculty = faculty)\
-                                .filter('term', major = major)\
-                                .filter('term', cohort = cohort)\
-                                .filter('term', entrance_year = entrance_year)\
-                                .filter('term', user_id = user_id)
-
-       
-       return query
-
+        return query
+    
+    @gen.coroutine
     def get_result(self, query, page, num):
         course_total = self.es_execute(query[:0]).hits.total 
-        if course_total == 0:
-            total_page = 0
-        elif course_total <= num:
-            total_page = 1
-        else:
-            if course_total%num == 0:
-                total_page = course_total/num
-            else:
-                total_page = course_total/num + 1
-        
+        total_page = self.get_total_page(course_total, num)
+
         results = self.es_execute(query[(page-1)*num:page*num]).hits
-        for result in results:
-            course_id = result.course_id
-            course_detail = yield self.course_detail('get', 'courses/detail', {'course_id': course_id})
-            image_url = course_detail['image_url']
-            course_info = result.to_dict()
-            course_info
+        data = yield self.add_course_image(results)
+        
+        raise gen.Return((course_info, total_page))
 
-        pass
-
+    @gen.coroutine
     def get(self):
         page = self.get_argument('page', 1)
         num = self.get_argument('num', 6)
         query = self.query
-        result = self.get_result(query, page, num)
-        self.success_response({'data': result})
+        result, total_page = yield self.get_result(query, page, num)
+        self.success_response({'data': result, 'total_page': total_page, 'current_page': page})
 
 
