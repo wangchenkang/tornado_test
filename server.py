7fb238b6c842754ccb42b43248535893c89f0858
wangchenkang <wangchenkang@xuetangx.com>
2018-03-21 12:55:17 +0000

    修复es重试机制

--- server.py
+++ server.py
@@ -35,8 +35,8 @@ class Application(tornado.web.Application):
             routed_handlers.append(
                 tornado.web.url(r'/static/(.*)', tornado.web.StaticFileHandler, {'path': app_settings['static_path']}))
 
-        self.es = connections.create_connection(hosts=settings.es_cluster, timeout=30)
-        self.moocnd_es = connections.create_connection(hosts=settings.moocnd_es_cluster, timeout=30)
+        self.es = connections.create_connection(hosts=settings.es_cluster, timeout=30, max_retries=3)
+        self.moocnd_es = connections.create_connection(hosts=settings.moocnd_es_cluster, timeout=30, max_retries=2)
         self.memcache = memcache.Client(settings.memcache_host)
 
         super(Application, self).__init__(routed_handlers, **app_settings)
