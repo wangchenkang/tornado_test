470cf6a148c75b8a1ecd66361d3f7446fef66b15
zhangyue <zhangyue@xuetangx.com>
2018-05-22 11:29:39 +0800

    学分课数据平台加course_id字段

--- handlers/credit_data.py
+++ handlers/credit_data.py
@@ -23,9 +23,9 @@ COURSE_OVERVIEW_TABLE_FIELD = ['course_name', 'course_id', 'enroll_num', 'use_pl
 COURSE_DETAIL_FIELD = ['enroll_num', 'society_enroll_num', 'platform_enroll_num', 'main_enroll_num', 'discussion_num', 'use_platform_num']
 COURSE_TABLE_FIELD = ['plat_name', 'enroll_num', 'avg_video_rate', 'avg_accmp_rate', 'discussion_num']
 PLATFORM_OVERVIEW_FIELD = ['plat_id', 'plat_name', 'credit_enroll_num', 'import_credit_num', 'course_status']
-PLATFORM_TABLE_FIELD = ['course_name', 'course_status', 'enroll_num', 'discussion_num', 'avg_score']
+PLATFORM_TABLE_FIELD = ['course_name', 'course_id', 'course_status', 'enroll_num', 'discussion_num', 'avg_score']
 
-PLATFORM_TABLE_NAME= [u'序号', u'课程名称', u'课程状态', u'学生人数', u'讨论区发回帖数', u'学生平均分']
+PLATFORM_TABLE_NAME= [u'序号', u'课程名称', u'课程id', u'课程状态', u'学生人数', u'讨论区发回帖数', u'学生平均分']
 COURSE_OVERVIEW_TABLE_NAME= [u'序号', u'课程名称', u'课程ID', u'学生总数', u'社会/平台/主站学生', u'使用平台数', u'课程状态']
 COURSE_TABLE_NAME= [u'序号', u'平台名称', u'选课人数(学生)', u'视频平均观看率', u'作业完成率', u'讨论回复数']
 PLATFORM_OVERVIEW_TABLE_NAME= [u'序号', u'平台id', u'平台名称', u'学分课学生人次', u'引进学分课数量', u'开课中/待开/已结课数量']
@@ -428,11 +428,11 @@ class CreditPlatformTable(CreditData):
         query = self.es_query(index=settings.CREDIT_ES_INDEX, doc_type='course_plat_detail') \
                     .filter('term', plat_id=plat_id) \
                     .filter(Q('bool', should=[Q('wildcard', course_name= '*%s*' % search_argument)])) \
-                    .source(['course_name', 'course_status', 'enroll_num', 'discussion_num','avg_score']) \
+                    .source(['course_name', 'course_status', 'enroll_num', 'discussion_num','avg_score', 'course_id']) \
                     .sort(sort_argument) 
         
-        query = self.make_query(query, download_status, page, num) 
-        
+        query = self.make_query(query, download_status, page, num)
+
         return query, page, num
 
     @gen.coroutine
@@ -447,6 +447,7 @@ class CreditPlatformTable(CreditData):
             l = list()
             l.append(index + 1)
             l.append(result.course_name)
+            l.append(result.course_id or '')
             status = self.convert_course_status(result.course_status)
             l.append(status)
             l.append(result.enroll_num or 0)
